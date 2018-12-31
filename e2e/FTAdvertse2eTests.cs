c59099bea8c36260b3a2bfc5e6768f6234c5cea2
Tomas <tcarneado@gmail.com>
2018-12-31 19:06:03 +0100

    e2e tests

---
+++ e2e/FTAdvertse2eTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+using FluentAssertions;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace e2e
+{
+    public class FTadvertse2eTests
+    {
+        public const string FTAdvertsApiRoot = "http://ftadvertsapi";        
+
+        [Fact]
+        public async Task IsHomeUp()
+        {
+            var client = new HttpClient();
+
+            var CheckApi = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{FTAdvertsApiRoot}")
+            };
+            Console.WriteLine($"Checking ftadverts.api: {CheckApi.RequestUri}");
+            using (var response = await client.SendAsync(CheckApi))
+            {
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                var actual = JObject.Parse(content);
+                var expected = JObject.Parse("{\"api\": \"ftadverts\"}");
+
+                actual.Should().BeEquivalentTo(expected);
+
+            }                     
+            
+        }
+   }
+}
