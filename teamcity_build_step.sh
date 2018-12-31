40e846b35a51a893503af4d5634c3ac5c6f9492d
Tomas <tcarneado@gmail.com>
2018-12-31 16:43:04 +0100

    registry_ip typo

--- teamcity_build_step.sh
+++ teamcity_build_step.sh
@@ -6,7 +6,7 @@ microservice="ftadverts/api"
 registry_ip=192.168.99.100
 registry_port=55000
 build_number=$1
-image=$registry_pi:$registry_port/$microservice:ci-$build_number
+image=$registry_ip:$registry_port/$microservice:ci-$build_number
 
 docker build -t $image .
 docker push $image
