9d7e04220d538ebac8ba134854d9abdba0590646
Tomas <tcarneado@gmail.com>
2019-01-04 19:40:01 +0100

    v5

--- api/Startup.cs
+++ api/Startup.cs
@@ -27,7 +27,7 @@ namespace api
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("{\"api\": \"ftadvertsv4\"}");
+                await context.Response.WriteAsync("{\"api\": \"ftadvertsv5\"}");
             });
         }
     }
