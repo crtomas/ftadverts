ddd2cf99252ebea9181cbd8965df740c56ce9f84
Tomas <tcarneado@gmail.com>
2019-01-22 00:15:31 +0100

    nash exit code

--- teamcity_build_step.sh
+++ teamcity_build_step.sh
@@ -21,3 +21,6 @@ echo $latest
 docker build -t $image -t $latest .
 docker push $image
 docker push $latest
+
+exit $(docker-compose ps -q | tr -d '[:space:]' |
+     xargs docker inspect -f '{{ .State.ExitCode }}' | grep -v 0 | wc -l | tr -d '[:space:]')
